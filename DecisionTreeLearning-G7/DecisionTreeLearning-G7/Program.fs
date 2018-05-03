18594e37687d506417238cefcfb17eb571216e52
dichha <dichha.chamling@gmail.com>
2018-05-03 01:04:17 -0500

    added csv

--- DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
+++ DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
@@ -2,12 +2,18 @@
 // See the 'F# Tutorial' project for more help.
 
 open FSharp.Data
-
-
+open FSharp.Data.CsvExtensions
+(*
 type DataModel = JsonProvider<"C:\\Users\\dichha\\Documents\\Graduate\\Spring2018\\AI\\final-project\\AI-Project\\DecisionTreeLearning-G7\\DecisionTreeLearning-G7\\data\\tic-tac-toe.json">
 let doc = DataModel.GetSample()
 printfn "Testing"
 printfn "%s" (doc.Attributes.``0``)
+*)
+let data = CsvFile.Load("C:\\Users\\dichha\\Documents\\Graduate\\Spring2018\\AI\\final-project\\AI-Project\\DecisionTreeLearning-G7\\DecisionTreeLearning-G7\\data\\tic-tac-toe.csv");
+
+
+for row in data.Rows do 
+   printfn "(%s)" (row.GetColumn "0")
 
 
 [<EntryPoint>]
