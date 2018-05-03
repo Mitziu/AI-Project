d9ab225835f4622cc9c8ad5defeea1e0f8b5a161
dichha <dichha.chamling@gmail.com>
2018-05-02 23:48:26 -0500

    tested json file is working fine

--- DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
+++ DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
@@ -1,6 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open FSharp.Data
+
+
+type DataModel = JsonProvider<"C:\\Users\\dichha\\Documents\\Graduate\\Spring2018\\AI\\final-project\\AI-Project\\DecisionTreeLearning-G7\\DecisionTreeLearning-G7\\data\\tic-tac-toe.json">
+let doc = DataModel.GetSample()
+printfn "Testing"
+printfn "%s" (doc.Attributes.``0``)
+
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
