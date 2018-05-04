22278feae7a8e3ebb5a4f3a8e6f3a1d666c5d3ab
dichha <dichha.chamling@gmail.com>
2018-05-03 23:52:52 -0500

    added json & data file for 2 dataset, json for 3rd dataset

--- DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
+++ DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
@@ -3,27 +3,65 @@
 
 open System.IO
 open FSharp.Data
+open FSharp.Data.JsonExtensions
+
+[<Literal>]
+let baseDirectory = __SOURCE_DIRECTORY__
+[<Literal>]
+let jsonFile1 = "\\Data\\tic-tac-toe.json"
+[<Literal>]
+let jsonFile2 = "\\Data\\balance.json"
+[<Literal>]
+let jsonFile3 = "\\Data\\breast-cancer.json"
+[<Literal>]
+let jsPath1 = baseDirectory + jsonFile1
+[<Literal>]
+let jsPath2 = baseDirectory + jsonFile2 
+[<Literal>]
+let jsPath3 = baseDirectory + jsonFile3 
 
 
 
-let jsonFile = "Data\\tic-tac-toe.json"
-type JsonData = JsonProvider<Sample=const(__SOURCE_DIRECTORY__ + "\\Data\\tic-tac-toe.json")>
+type JsonData = JsonProvider<jsPath3>
 let doc = JsonData.GetSample()
+printfn "Testing"
 printfn "%s" doc.Attributes.``0`` // attribute name by value
 let list = doc.Domain.``0``
 printfn "%A" list // attribute index 0's domain
 
 
+let readLines filePath = Seq.toList (System.IO.File.ReadLines(filePath))
 
-let baseDirectory = __SOURCE_DIRECTORY__
-let filePath = "Data\\tic-tac-toe.txt"
-let fullPath = Path.Combine(baseDirectory, filePath)
+(*
+//1:  tic-tac-toe
+let filePath1 = "Data\\tic-tac-toe.txt"
+let fullPath1 = Path.Combine(baseDirectory, filePath1)
 
 // method for converting sequece to list
+let result1' = readLines fullPath1 
+let result1 = result1' |> List.map (fun l -> l.Split ',' |> Array.toList) 
+*)
+(*
+let swap (array: 'a[]) (first: int) (second: int): 'a[] = 
+    let originalFirst = array.[first]
+    array.[first] <- array.[second];
+    array.[second] <- originalFirst;
+    array
+*)
+// 2. balance 
+(*
+let shift (array: 'a[]) = 
+    let cat = array.[0]
+    for i in 1..array.Length-1 do 
+        array.[i-1] <- array.[i]
+    array.[array.Length-1] <- cat
+    array
+let filePath2 = "Data\\balance.txt"
+let fullPath2 = Path.Combine(baseDirectory, filePath2)
+let result2' = readLines fullPath2 
+let result2 = result2' |> List.map (fun a -> a.Split ','|> fun arr -> shift arr |> Array.toList)
+*)
 
-let readLines filePath = Seq.toList (System.IO.File.ReadLines(filePath))
-let result' = readLines fullPath 
-let result = result' |> List.map (fun l -> l.Split ',' |> Array.toList) 
 
 
 [<EntryPoint>]
--- DecisionTreeLearning-G7/DecisionTreeLearning-G7/data/data-cleaning.py
+++
@@ -1,5 +0,0 @@
-import urllib2
-link = "https://archive.ics.uci.edu/ml/machine-learning-databases/tic-tac-toe/tic-tac-toe.data"
-reponse = urllib2.urlopen(link)
-content = response.read()
-print(content)
