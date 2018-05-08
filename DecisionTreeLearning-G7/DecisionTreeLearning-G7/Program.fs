1bc7bc6d6c567059e62deefd92022459479cb8e2
dichha <dichha.chamling@gmail.com>
2018-05-08 13:13:35 -0500

    added 2 more dataset, removed the one w/ 3 classifier

--- DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
+++ DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
@@ -10,25 +10,32 @@ let baseDirectory = __SOURCE_DIRECTORY__
 [<Literal>]
 let jsonFile1 = "\\Data\\tic-tac-toe.json"
 [<Literal>]
-let jsonFile2 = "\\Data\\balance.json"
+let jsonFile2 = "\\Data\\house-votes.json"
 [<Literal>]
 let jsonFile3 = "\\Data\\breast-cancer.json"
+[<Literal>]
+let jsonFile4 = "\\Data\\monk.json"
+
 [<Literal>]
 let jsPath1 = baseDirectory + jsonFile1
 [<Literal>]
 let jsPath2 = baseDirectory + jsonFile2 
 [<Literal>]
 let jsPath3 = baseDirectory + jsonFile3 
+[<Literal>]
+let jsPath4 = baseDirectory + jsonFile4
 
 
 
-type JsonData = JsonProvider<jsPath3>
+
+type JsonData = JsonProvider<jsPath2>
 let doc = JsonData.GetSample()
 printfn "Testing"
 printfn "%s" doc.Attributes.``0`` // attribute name by value
 let list = doc.Domain.``0``
 printfn "%A" list // attribute index 0's domain
 
+// mapping col # to its attribute
 
 let readLines filePath = Seq.toList (System.IO.File.ReadLines(filePath))
 
@@ -48,7 +55,7 @@ let swap (array: 'a[]) (first: int) (second: int): 'a[] =
     array.[second] <- originalFirst;
     array
 *)
-// 2. balance 
+// 2. house-votes
 
 let shift (array: 'a[]) = 
     let cat = array.[0]
@@ -57,20 +64,28 @@ let shift (array: 'a[]) =
     array.[array.Length-1] <- cat
     array
 (*
-let filePath2 = "Data\\balance.txt"
+// 2: house-votes
+let filePath2 = "Data\\house-votes.txt"
 let fullPath2 = Path.Combine(baseDirectory, filePath2)
 let result2' = readLines fullPath2 
 let result2 = result2' |> List.map (fun a -> a.Split ','|> fun arr -> shift arr |> Array.toList)
-*)
+
 
 // 3. breast-cancer 
-(*
+
 let filePath3 = "Data\\breast-cancer.txt" 
 let fullPath3 = Path.Combine(baseDirectory, filePath3)
 let result3' = readLines fullPath3
 let result3 = result3' |> List.map (fun a -> a.Split ',' |> fun arr -> shift arr |> Array.toList)
-*)
 
+
+// 4. monk 
+
+let filePath4 = "Data\\monk.txt"
+let fullPath4 = Path.Combine(baseDirectory, filePath4)
+let result4' = readLines fullPath4
+let result4 = result4' |> List.map (fun a -> a.Split ' ' |> Array.filter ((<>) "")|> fun arr -> shift arr |> Array.toList)
+*)
 
 
 
