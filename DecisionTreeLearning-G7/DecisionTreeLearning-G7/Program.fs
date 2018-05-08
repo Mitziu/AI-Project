d2173399fb192b1fc25fa4c2658b8b23554862a3
dichha <dichha.chamling@gmail.com>
2018-05-08 13:19:45 -0500

    removed unnecessary functions

--- DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
+++ DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
@@ -28,9 +28,10 @@ let jsPath4 = baseDirectory + jsonFile4
 
 
 
-type JsonData = JsonProvider<jsPath2>
+type JsonData = JsonProvider<jsPath1>
 let doc = JsonData.GetSample()
 printfn "Testing"
+
 printfn "%s" doc.Attributes.``0`` // attribute name by value
 let list = doc.Domain.``0``
 printfn "%A" list // attribute index 0's domain
@@ -40,22 +41,6 @@ printfn "%A" list // attribute index 0's domain
 let readLines filePath = Seq.toList (System.IO.File.ReadLines(filePath))
 
 (*
-//1:  tic-tac-toe
-let filePath1 = "Data\\tic-tac-toe.txt"
-let fullPath1 = Path.Combine(baseDirectory, filePath1)
-
-// method for converting sequece to list
-let result1' = readLines fullPath1 
-let result1 = result1' |> List.map (fun l -> l.Split ',' |> Array.toList) 
-*)
-(*
-let swap (array: 'a[]) (first: int) (second: int): 'a[] = 
-    let originalFirst = array.[first]
-    array.[first] <- array.[second];
-    array.[second] <- originalFirst;
-    array
-*)
-// 2. house-votes
 
 let shift (array: 'a[]) = 
     let cat = array.[0]
@@ -63,7 +48,17 @@ let shift (array: 'a[]) =
         array.[i-1] <- array.[i]
     array.[array.Length-1] <- cat
     array
-(*
+
+
+//1:  tic-tac-toe
+let filePath1 = "Data\\tic-tac-toe.txt"
+let fullPath1 = Path.Combine(baseDirectory, filePath1)
+
+// method for converting sequece to list
+let result1' = readLines fullPath1 
+let result1 = result1' |> List.map (fun l -> l.Split ',' |> Array.toList) 
+
+
 // 2: house-votes
 let filePath2 = "Data\\house-votes.txt"
 let fullPath2 = Path.Combine(baseDirectory, filePath2)
