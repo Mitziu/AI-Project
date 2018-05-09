e537cbe5c87756c1fbc3548d612623f01bc84893
dichha <dichha.chamling@gmail.com>
2018-05-08 21:08:24 -0500

    json format as required by algo

--- DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
+++ DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
@@ -4,6 +4,13 @@
 open System.IO
 open FSharp.Data
 open FSharp.Data.JsonExtensions
+open System.Runtime.Serialization.Formatters
+open System.Runtime.Serialization.Formatters
+open System.Security.Cryptography
+open System.ComponentModel
+open FSharp.Json
+open System.Diagnostics
+open System.Collections.Generic
 
 [<Literal>]
 let baseDirectory = __SOURCE_DIRECTORY__
@@ -26,17 +33,67 @@ let jsPath3 = baseDirectory + jsonFile3
 let jsPath4 = baseDirectory + jsonFile4
 
 
-
-
-type JsonData = JsonProvider<jsPath1>
+type JsonData = JsonProvider<jsPath4>
 let doc = JsonData.GetSample()
-printfn "Testing"
 
-printfn "%s" doc.Attributes.``0`` // attribute name by value
-let list = doc.Domain.``0``
-printfn "%A" list // attribute index 0's domain
+// for splitting attributes
+let splitAttr (text: string) = 
+    let mutable cleanText = text
+    cleanText <- cleanText.Replace("(", "")
+    cleanText <- cleanText.Replace(")", "")
+    cleanText <- cleanText.Replace("\"", "")
+    let word = cleanText.Split ','
+    word
 
-// mapping col # to its attribute
+// for splitting domains
+let splitDomain (text: string) = 
+    let mutable cleanText = text
+    cleanText <- cleanText.Replace("(", "")
+    cleanText <- cleanText.Replace(")", "")
+    cleanText <- cleanText.Replace("\"", "")
+    cleanText <- cleanText.Replace("\"", "")
+    let word = cleanText.Split '['
+    word
+
+// FOR ATTRIBUTES PROCESSING
+let attrs = doc.Attributes.JsonValue.Properties
+
+let attrList = new List<string>()
+for attr in attrs do 
+   attrList.Add(attr.ToString())
+
+
+let mutable attrMap = Map.empty
+let mutable tempMap = Map.empty
+for item in attrList do 
+    //printfn "testing"
+    let result = splitAttr item //splitLine item
+    attrMap <- attrMap.Add((result.[1]).Trim(), (result.[0]).Trim())
+    tempMap <- tempMap.Add((result.[0]).Trim(), (result.[1]).Trim())
+
+
+// FOR DOMAIN PROCESSING
+let domains = doc.Domain.JsonValue.Properties
+
+let domainList = new List<string>()
+for domain in domains do 
+    domainList.Add(domain.ToString())
+
+let mutable domMap = Map.empty
+printfn "testing"
+
+for item in domainList do 
+    let result = splitDomain item
+    // printfn "%A" result
+    //printfn "%s" ((result.[0].Replace(",", "").Trim()))
+    let key = result.[0].Replace(",", "").Trim()
+    //printfn "%s" (result.[2])
+    //printfn "%s" (tempMap.Item(key))
+    let value = result.[1].Replace("]", "").Trim().Split ',' |> List.ofArray
+   //printfn "%A" value
+
+    domMap <- domMap.Add(tempMap.Item(key), value)
+
 
 let readLines filePath = Seq.toList (System.IO.File.ReadLines(filePath))
 
