607233dea904edbd2e390febd7944be40b58a8ad
dichha <dichha.chamling@gmail.com>
2018-05-10 00:30:48 -0500

    json map prep -- success

--- DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
+++ DecisionTreeLearning-G7/DecisionTreeLearning-G7/Program.fs
@@ -3,11 +3,15 @@
 
 open System.IO
 open FSharp.Data
+open FSharp.Data.JsonExtensions
 open System.Collections.Generic
-
+open System
+// gets base path to be used fo relative path 
 [<Literal>]
 let baseDirectory = __SOURCE_DIRECTORY__
 [<Literal>]
+
+// 5 json files path 
 let jsonFile1 = "\\Data\\tic-tac-toe.json"
 [<Literal>]
 let jsonFile2 = "\\Data\\house-votes.json"
@@ -18,7 +22,7 @@ let jsonFile4 = "\\Data\\monk.json"
 [<Literal>]
 let jsonFile5 = "\\Data\\spect.json"
 
-
+// path for the json files
 [<Literal>]
 let jsPath1 = baseDirectory + jsonFile1
 [<Literal>]
@@ -30,10 +34,14 @@ let jsPath4 = baseDirectory + jsonFile4
 [<Literal>]
 let jsPath5 = baseDirectory + jsonFile5
 
+// for splitting attributes
+let splitAttr (text: string) = 
+    let mutable cleanText = text
+    cleanText <- cleanText.Replace("(", "").Replace(")", "").Replace("\"", "")
+    let word = cleanText.Split ','
+    word
 
-//type JsonData = JsonProvider<jsPath5>
 // for splitting domains
-//printfn "Testing for splitting"
 let splitDomain (text: string) = 
     let mutable cleanText = text
     cleanText <- cleanText.Replace("(", "").Replace(")", "").Replace("\"", "").Replace("]", "").Replace("\r\n", "")
@@ -41,12 +49,36 @@ let splitDomain (text: string) =
     //printfn "%A" word
     word
 
-(*
-let domainMapping jsonFile = 
-    type Data = new JsonProvider<jsonFile>
-    let doc = JsonData.GetSample()
+// for mapping domains and attributes
+// @jsonFilePath: path of the json file
+let domainMapping jsonFilePath = 
+    //type JsonData = JsonProvider<jsonFilePath>
+    //printfn "testing"
+    let data = File.ReadAllText(jsonFilePath)
+    let doc = JsonValue.Parse(data)
+    //printfn "testing"
+    //printfn "%s" (doc.ToString())
+ 
+    let attributes = doc?attributes
+    let attrs = attributes.Properties
+
+    let attrList = new List<string>()
+    for attr in attrs do 
+        attrList.Add(attr.ToString())
+    let mutable indexMap = Map.empty
+    let mutable tempMap = Map.empty
+
+    for item in attrList do 
+        //printfn "testing"
+        let result = splitAttr item //splitLine item
+        indexMap <- indexMap.Add((result.[1]).Trim(), (result.[0]).Trim() |> int)
+        tempMap <- tempMap.Add((result.[0]).Trim() |> int, (result.[1]).Trim())
+    //printfn "printing indexMap"
+    //printfn "%A" indexMap
+    
     // FOR DOMAIN PROCESSING
-    let domains = doc.Domain.JsonValue.Properties
+    let dom = doc?domain
+    let domains = dom.Properties
     let domainList = new List<string>()
     for domain in domains do 
        domainList.Add(domain.ToString())
@@ -62,27 +94,8 @@ let domainMapping jsonFile =
         let value = result.[1].Split ',' |> Array.toList |> List.map (fun x -> x.Trim())
         //printfn "%A" value
         domMap <- domMap.Add(tempMap.Item(key), value)
-// for splitting attributes
-let splitAttr (text: string) = 
-    let mutable cleanText = text
-    cleanText <- cleanText.Replace("(", "").Replace(")", "").Replace("\"", "")
-    let word = cleanText.Split ','
-    word
-// FOR ATTRIBUTES PROCESSING
-let attrs = doc.Attributes.JsonValue.Properties
-let attrList = new List<string>()
-for attr in attrs do 
-   attrList.Add(attr.ToString())
-let mutable indexMap = Map.empty
-let mutable tempMap = Map.empty
-for item in attrList do 
-    //printfn "testing"
-    let result = splitAttr item //splitLine item
-    indexMap <- indexMap.Add((result.[1]).Trim(), (result.[0]).Trim() |> int)
-    tempMap <- tempMap.Add((result.[0]).Trim() |> int, (result.[1]).Trim())
-*)
-
-
+    (domMap, indexMap)
+   
 
 // method for converting sequece to list
 let readLines filePath = Seq.toList (System.IO.File.ReadLines(filePath))
@@ -93,17 +106,18 @@ let shift (array: 'a[]) =
     array.[array.Length-1] <- cat
     array
 
+// get dataset for tic-tac-toe
+// @datasetFileName: name of a dataset
 let getTTTData datasetFileName = 
     //1:  tic-tac-toe
     let filePath1 = "Data\\" + datasetFileName
     let fullPath1 = Path.Combine(baseDirectory, filePath1)
-
-
     let result1' = readLines fullPath1 
     let result1 = result1' |> List.map (fun l -> l.Split ',' |> Array.toList) 
     result1
 
-
+// get dataset for monk
+// @datasetFileName: name of a dataset
 let getMonkData datasetFileName = 
     // 4. monk 
     let filePath4 = "Data\\" + datasetFileName
@@ -112,48 +126,57 @@ let getMonkData datasetFileName =
     let result4 = result4' |> List.map (fun a -> a.Split ' ' |> Array.filter ((<>) "")|> fun arr -> shift arr |> Array.toList)
     result4
 
-let getHVBCData datasetFileName = 
+// get dataset for spect, house-votes & breast-cancer 
+// @datasetFileName: name of a dataset
+let getSHVBCData datasetFileName = 
     // 2: house-votes
     let filePath2 = "Data\\" + datasetFileName
     let fullPath2 = Path.Combine(baseDirectory, filePath2)
     let result2' = readLines fullPath2 
     let result2 = result2' |> List.map (fun a -> a.Split ','|> fun arr -> shift arr |> Array.toList)
     result2
-
+// matches dataset file name w/ it's appropriate data parser
+// @datasetFileName: name of a dataset
 let getSanitizedData datasetFileName = 
     let mutable dataset = []
     if (datasetFileName = "tic-tac-toe.txt") then  dataset <- (getTTTData datasetFileName)
     else if (datasetFileName = "monk.txt") then dataset <- (getMonkData datasetFileName)
-    else if (datasetFileName = "house-votes.txt" || datasetFileName = "breast-cancer.txt" || datasetFileName = "spect.txt") then dataset <- (getHVBCData datasetFileName)
+    else if (datasetFileName = "house-votes.txt" || datasetFileName = "breast-cancer.txt" || datasetFileName = "spect.txt") then dataset <- (getSHVBCData datasetFileName)
     else
         printfn "No dataset found with the provided filename." 
     dataset
-(*
-let getDomainMap jsonFileName = 
-    let mutable domMap = Map.empty
+
+// maps json file name w/ its appropriate file path and calls method for getting map of domain and attributes
+// @jsonFileName: name of a json file
+let getMaps jsonFileName = 
     let mutable path = ""
     if (jsonFileName = "tic-tac-toe.json") then path <- jsPath1
     else if (jsonFileName = "monk.json") then path <- jsPath4
     else if (jsonFileName = "house-votes.json") then path <- jsPath2
-    else if (jsonFileName = "breast-cancer.txt") then path <- jsPath3
+    else if (jsonFileName = "breast-cancer.json") then path <- jsPath3
+    else if (jsonFileName = "spect.json") then path <- jsPath5
     else
         printfn "No json file found with the provided filename." 
-        
-    domMap <- domainMapping path 
-*)
-
-
+ 
+    let attDomTup = domainMapping path
+    attDomTup  
+ 
 
 
 
 [<EntryPoint>]
 let main argv = 
+    
     let jsonFile = argv.[0].Trim()
     let dataFile = argv.[1].Trim()
     let dataset = getSanitizedData dataFile 
-    //let domMap = getDomainMap jsonFile
+    let domMap, indexMap = getMaps jsonFile
+
+    printfn "%A" domMap
+    printfn "%A" indexMap
     printfn "%A" dataset
     printfn "%s" jsonFile
     printfn "%s" dataFile
+    //printfn "%A" (domMap.Item("a1"))
     //printfn "%A" argv
     0 // return an integer exit code
